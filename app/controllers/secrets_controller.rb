850bd3ab7f9d3a4aa04bd12fdc2c2c254665fd07
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-02-12 14:54:17 -0700

    create secrets controller, add user, made user model

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,19 @@
+class SecretsController < ApplicationController
+  before_action :authenticate
+
+  def index
+  end
+
+  private
+  def authenticate
+    redirect to '/login' unless current_user
+  end
+
+  def current_user
+
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+end
