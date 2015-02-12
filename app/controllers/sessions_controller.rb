e4d7adb1e7863ebf897e314f429e1228b0b34dd1
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-02-12 15:12:16 -0700

    modify routes, create sessions controller, create new views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to secrets_path
+    else
+      flash[:alert] = "DENIED!"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :secrets
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'secrets#create'
+  delete '/logout' => 'sessions#destroy' 
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
